cf68403f35ea5c176a2a3a18e327f6d5017f39ea
Jorge Machado <jorge.ltmch@eduge.ch>
2020-02-26 14:56:53 +0100

    post d'images fini

--- lib.inc.php
+++ lib.inc.php
@@ -8,11 +8,15 @@
     require_once 'EDatabase.php';
 
 
+    function getPost(){
+        
+    }
+
 
 
     function addPost($titre, $description, $typeMedia, $nomFichier, $sizeFichier, $tmpNameFichier){
         $sql = "INSERT INTO Post(titrePost, descriptionPost, dateCreationPost, dateModificationPost) VALUES(:titrePost, :descriptionArticle, :dateCrea, :dateModif)";
-        $req = EDatabase::prepare($sql, array(PDO::ATTR_CURSOR, PDO::CURSOR_SCROLL));
+        $req = EDatabase::prepare($sql);
         $req->execute(
           array(
              'titrePost' => $titre,
@@ -25,29 +29,45 @@
 
 
         $sql = "INSERT INTO Media(typeMedia, nomFichierMedia, dateCreationMedia, dateModificationMedia, idPost) VALUES(:typeMedia, :nomFichier, :dateCrea, :dateModif, :post)";
-        $req = EDatabase::prepare($sql, array(PDO::ATTR_CURSOR, PDO::CURSOR_SCROLL));
-        $req->execute(
-          array(
-             'typeMedia' => $typeMedia,
-             'nomFichier' => $nomFichier,
-             'dateCrea' => date("Y-m-d H:i:s"),
-             'dateModif' => date("Y-m-d H:i:s"),
-             'post' => $id
-             )
-         );  
-         
-         addMediaToServer($typeMedia, $tmpNameFichier, $sizeFichier);
+
+        for ($i=0; $i < count($nomFichier); $i++) { 
+            $req = EDatabase::prepare($sql);
+            $req->execute(
+              array(
+                 'typeMedia' => $typeMedia[$i],
+                 'nomFichier' => $nomFichier[$i],
+                 'dateCrea' => date("Y-m-d H:i:s"),
+                 'dateModif' => date("Y-m-d H:i:s"),
+                 'post' => $id
+                 )
+             );  
+             
+             addMediaToServer($nomFichier[$i], $typeMedia[$i], $tmpNameFichier[$i], $sizeFichier[$i]);
+        }
+        
     }
 
 
     //TODO : REMETTRE BIEN LES FONCTIONS
-    function addMediaToServer($typeFichier, $tmpName, $sizeFichier){
+    function addMediaToServer($nomFichier, $typeFichier, $tmpName, $sizeFichier, $cpt = 0){
         $typesAcceptes = array("image/gif", "image/png", "image/jpeg", "video/mp4", "audio/mpeg"); //PAS SECURISE, A SECURISER
 
 
-        if (in_array($typeFichier, $typesAcceptes)) {
-            move_uploaded_file($tmpName, "/img");
-        }
+
+            if (in_array($typeFichier, $typesAcceptes)) {
+                if (file_exists("./img/" . $nomFichier)) {
+
+                    $cpt++;
+                    $temp = explode(".", $nomFichier);
+                    
+                    addMediaToServer( $cpt . $nomFichier  , $typeFichier, $tmpName, $sizeFichier, $cpt);
+                }
+                else{
+                    
+                    move_uploaded_file($tmpName, "./img/" . $nomFichier);
+                }    
+            }
+
 
 
     }
