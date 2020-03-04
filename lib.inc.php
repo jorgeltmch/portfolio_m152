76d10dcf062412e828a4052b02dfdec2280fda6b
Jorge Machado <jorge.ltmch@eduge.ch>
2020-03-04 13:29:02 +0100

    changements mineurs

--- lib.inc.php
+++ lib.inc.php
@@ -19,21 +19,23 @@ function displayPosts($posts)
             <div class=\"uk-card-header\">\n
                             <div class=\"uk-grid-small uk-flex-middle\" uk-grid>\n
                                 <div class=\"uk-width-auto\">\n
-                                    <img class=\"uk-border-circle\" width=\"40\" height=\"40\" src=\"img/avatar.jpg\">\n
+                                    <img class=\"uk-border-circle\" width=\"40\" height=\"40\" src=\"img/avatar.jpg\"> \n
                                 </div>\n
                                 <div class=\"uk-width-expand\">\n
-                                    <h3 class=\"uk-card-title uk-margin-remove-bottom\">" . $post["titrePost"] . "</h3>\n
-                                    <p class=\"uk-text-meta uk-margin-remove-top\"><time datetime=\"2016-04-01T19:00\">" . $post["dateCreationPost"] . "</time></p>\n
+                                    <h3 class=\"uk-card-title uk-margin-remove-bottom\">" . $post["titrePost"] . "</h3> \n
+                                    <p class=\"uk-text-meta uk-margin-remove-top\"><time datetime=\"2016-04-01T19:00\">" . $post["dateCreationPost"] . "</time> </p>\n
                                 </div>\n
                             </div>\n
-                        </div>
-                        <div class=\"uk-card-media-top\">\n
-                                    <img src=\"img/" . $post["nomFichierMedia"] . "\" width=\"100%\" height=\"100%\">\n
-                                </div>\n
+                        </div>";
 
+                        if (!empty($post["nomFichierMedia"])) {
+                            $html .=  "<div class=\"uk-card-media-top\">\n
+                            <img src=\"img/" . $post["nomFichierMedia"] . "\" width=\"100%\" height=\"100%\">\n
+                        </div>\n";
+                        }
                                 
-                        <div class=\"uk-card-body\">\n
-                            <p>" . $post["descriptionPost"] . "</p>\n
+                        $html .= "<div class=\"uk-card-body\">\n
+                            <p>" . $post["descriptionPost"] . " <a  class=\"uk-align-right\" href=\"\" uk-icon=\"icon: pencil\"> </a> <a class=\"uk-align-right\" href=\"\" uk-icon=\"icon: trash\"> </a></p> \n
                         </div>\n
             </div>\n
         </div>\n";
@@ -46,7 +48,7 @@ function getPosts()
 {
     static $req;
 
-    $sql = 'SELECT titrePost, descriptionPost, dateCreationPost, dateModificationPost, nomFichierMedia FROM Post INNER JOIN Media ON Post.idPost = Media.idPost'; //AND rendu = 1
+    $sql = 'SELECT titrePost, descriptionPost, dateCreationPost, dateModificationPost, nomFichierMedia FROM Post INNER JOIN Media ON Post.idPost = Media.idPost ORDER BY dateCreationPost DESC'; //AND rendu = 1
     if ($req == null) {
         try {
             $req = EDatabase::prepare($sql); //plus rapide qu'un query
@@ -60,9 +62,21 @@ function getPosts()
 }
 
 
+function addPostAndMedia(){
+    EDatabase::beginTransaction();
+
+    //SI PAS OK
+    EDatabase::rollBack();
+
+    //SI OK
+    EDatabase::commit(); 
+
+}
+
 
 function addPost($titre, $description, $typeMedia, $nomFichier, $sizeFichier, $tmpNameFichier)
 {
+    
     $sql = "INSERT INTO Post(titrePost, descriptionPost, dateCreationPost, dateModificationPost) VALUES(:titrePost, :descriptionArticle, :dateCrea, :dateModif)";
     $req = EDatabase::prepare($sql);
     $req->execute(
